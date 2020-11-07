ac64912d064efffe27d704b72899267d21e03c27
Benjamin Bourgeois <benjishift@gmail.com>
2020-11-07 15:24:12 +0100

    :sparkles: fixed environnment variable and add register and login user with token

--- server/routes/account/account-routes.js
+++ server/routes/account/account-routes.js
@@ -7,14 +7,35 @@ const JWTService = require("../../services/JWTToken");
 // register a user
 router.post('/register', function(req, res) {
     pool.getPool().query("INSERT INTO users (username, password, email, activate_email) VALUES ($1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id", [req.query.username, req.query.password, req.query.email, false], (err, result) => {
-        if (result.rows.empty()) {
-            console.debug("REGISTER : User already exist.")
-            res.sendStatus(409)
+        if (err) {
+            res.status(503);
+            res.json({message: "Service Unavailable"})
         } else {
-            const token = JWTService.generateAccessToken({ user_id: 1 });
-            res.json(token);
+            if (!result.rows.length) {
+                console.debug("REGISTER : User already exist.")
+                res.sendStatus(409)
+            } else {
+                res.sendStatus(200);
+            }
         }
     })
 });
+
+router.post('/login', function (req, res) {
+    pool.getPool().query("SELECT id FROM users WHERE username = $1 OR email = $1 AND password = $2", [req.query.identifier, req.query.password], (err, result) => {
+        if (err) {
+            res.status(503);
+            res.json({message: "Service Unavailable"})
+        } else {
+            if (!result.rows.length) {
+                res.status(401);
+                res.json({message: "User not found."})
+            } else {
+                const token = JWTService.generateAccessToken({user_id: result.rows[0].id})
+                res.json({JWTToken: token})
+            }
+        }
+    })
+})
 
 module.exports = router;
