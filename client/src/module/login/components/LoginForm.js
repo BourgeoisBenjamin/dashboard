16a6fab3ee706e02f8a6bc46a65973ba6fc8117c
Amaury <amaury.lecomte@epitech.eu>
2020-11-07 15:03:27 +0100

    :sparkles: Added login module UI

--- client/src/module/login/components/LoginForm.js
+++ client/src/module/login/components/LoginForm.js
@@ -3,6 +3,8 @@ import './LoginForm.css';
 import { FormControl, InputLabel, OutlinedInput, InputAdornment } from '@material-ui/core';
 import LockIcon from '@material-ui/icons/Lock';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import youtubeImage from '../../../assets/images/youtube.png';
+import twitterImage from '../../../assets/images/twitter.png';
 
 export class LoginForm extends Component {
     render() {
@@ -32,8 +34,27 @@ export class LoginForm extends Component {
                         />
                     </FormControl>
                 </div>
+                <div className="forgot-password">
+                    <p>Forgot your password ?</p>
+                </div>
                 <div class="button-login">
                     <button>Login</button>
+                </div>
+                <div class="tiers-service-connect">
+                    <div class="title">
+                        <p>Or with</p>
+                    </div>
+                    <div class="logos">
+                        <div className="twitter logo">
+                            <img src={twitterImage} alt=""/>
+                        </div>
+                        <div className="youtube logo">
+                            <img src={youtubeImage} alt=""/>
+                        </div>
+                    </div>
+                </div>
+                <div className="button-sign-up">
+                    <p>Sign up</p>
                 </div>
             </div>
         );
