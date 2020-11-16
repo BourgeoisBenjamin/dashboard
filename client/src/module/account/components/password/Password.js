feb867785211861ba7b81a07ca52747cb6c38bc5
Amaury <amaury.lecomte@epitech.eu>
2020-11-16 14:32:44 +0100

    :lipstick: Added account password parameters ui

--- client/src/module/account/components/password/Password.js
+++ client/src/module/account/components/password/Password.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import './Password.css'
+import {FormControl, InputAdornment, InputLabel, OutlinedInput} from "@material-ui/core";
+import LockIcon from "@material-ui/icons/Lock";
 
 class Password extends Component
 {
@@ -14,8 +16,55 @@ class Password extends Component
 
     render() {
         return (
-            <div id="password">
-                <p>Password</p>
+            <div id="password-account">
+                <div className="element">
+                    <div className="first-column">
+                        <div className="title">
+                            <p>Password</p>
+                        </div>
+                        <div className="description">
+                            <p>Change your password</p>
+                        </div>
+                    </div>
+                    <div className="second-column">
+                        <div className="input">
+                            <FormControl fullWidth variant="outlined">
+                                <InputLabel htmlFor="outlined-adornment-amount">Old password</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start"><LockIcon/></InputAdornment>}
+                                    labelWidth={100}
+                                    type="password"
+                                />
+                            </FormControl>
+                        </div>
+                        <div className="input">
+                            <FormControl fullWidth variant="outlined">
+                                <InputLabel htmlFor="outlined-adornment-amount">New password</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start"><LockIcon/></InputAdornment>}
+                                    labelWidth={120}
+                                    type="password"
+                                />
+                            </FormControl>
+                        </div>
+                        <div className="input">
+                            <FormControl fullWidth variant="outlined">
+                                <InputLabel htmlFor="outlined-adornment-amount">Confirm new password</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    startAdornment={<InputAdornment position="start"><LockIcon/></InputAdornment>}
+                                    labelWidth={200}
+                                    type="password"
+                                />
+                            </FormControl>
+                        </div>
+                        <div className="button">
+                            <button onClick={this.onClickRegister}>Update</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
