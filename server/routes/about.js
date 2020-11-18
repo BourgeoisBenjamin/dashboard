1ef689a47ab69e9f11fd3548483657b62fca1b1a
Benjamin Bourgeois <benjishift@gmail.com>
2020-11-18 21:48:11 +0100

    :construction: continu /about.json

--- server/routes/about.js
+++ server/routes/about.js
@@ -2,13 +2,25 @@ const router = require('express').Router();
 
 // about.json file
 router.get('/about.json', function(req, res) {
+
     const client_ip = {host: req.ip}
-    const current_time = {current_time: Date.now()}
-    const server = [];
-    server.push()
+    const services = []
+    const server = {};
+
+    services.push({name: 'weather', widgets: [
+            {
+                name: 'city_temperature'
+            }
+        ]})
+
+
+    server.customer = client_ip
+    server.server = {current_time: Date.now(), services: services}
+
+
 
     res.status(200)
-    res.json({customer: client_ip, server:(current_time)});
+    res.json(server);
 });
 
 module.exports = router;
