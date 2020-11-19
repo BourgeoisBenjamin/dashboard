8b748962e6c0635f29946849998d589fa4b6b013
Benjamin Bourgeois <benjishift@gmail.com>
2020-11-19 10:09:39 +0100

    :sparkles: added /about.json

--- server/routes/about.js
+++ server/routes/about.js
@@ -1,26 +1,15 @@
 const router = require('express').Router();
+const aboutFile = require('../config/about.json')
+const fs = require('fs');
 
 // about.json file
 router.get('/about.json', function(req, res) {
 
-    const client_ip = {host: req.ip}
-    const services = []
-    const server = {};
-
-    services.push({name: 'weather', widgets: [
-            {
-                name: 'city_temperature'
-            }
-        ]})
-
-
-    server.customer = client_ip
-    server.server = {current_time: Date.now(), services: services}
-
-
+    aboutFile.customer.host = req.ip;
+    aboutFile.server.current_time = Date.now();
 
     res.status(200)
-    res.json(server);
+    res.json(aboutFile);
 });
 
 module.exports = router;
