9f29436503adebc0cee66d2cb77eb5948ce36acc
Benjamin Bourgeois <benjishift@gmail.com>
2020-11-23 10:11:56 +0100

    :rocket: deploy route in order to get actual params of meteo widgets and add security level

--- server/routes/service/weather-widget-routes.js
+++ server/routes/service/weather-widget-routes.js
@@ -4,22 +4,32 @@ const JWTService = require("../../services/JWTToken");
 const weather = require('openweather-apis');
 
 router.delete('/weather/city-meteo/:id_widget', JWTService.authenticateToken, function (req, res) {
-    pool.getPool().query("DELETE FROM city_meteo_weather WHERE id = $1 AND id_weather_service IN (SELECT id FROM weather_service WHERE id_user = $2)", [req.params.id_widget, req.user.user_id], (err, result) => {
+    pool.getPool().query("DELETE FROM city_meteo_weather WHERE id = $1 AND id_weather_service IN (SELECT id FROM weather_service WHERE id_user = $2) RETURNING *", [req.params.id_widget, req.user.user_id], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            res.status(200);
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+            }
         }
     })
 })
 
 router.put('/weather/city-meteo/:id_widget', JWTService.authenticateToken, function (req, res) {
-    pool.getPool().query("UPDATE city_meteo_weather SET activate = $3, city = $4, celsius = $5 WHERE id = $1 AND id_weather_service IN (SELECT id FROM weather_service WHERE id_user = $2)", [req.params.id_widget, req.user.user_id, req.query.activated, req.query.city, req.query.celsius], (err, result) => {
+    pool.getPool().query("UPDATE city_meteo_weather SET activate = $3, city = $4, celsius = $5 WHERE id = $1 AND id_weather_service IN (SELECT id FROM weather_service WHERE id_user = $2) RETURNING id", [req.params.id_widget, req.user.user_id, req.query.activated, req.query.city, req.query.celsius], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+                return;
+            }
             res.status(200);
         }
     })
@@ -29,11 +39,16 @@ router.get('/weather/city-meteo/:id_widget', JWTService.authenticateToken, funct
 
     let widgetInfos;
 
-    pool.getPool().query("SELECT w.city, w.celsius, s.api_key FROM city_meteo_weather w INNER JOIN weather_service s ON w.id_weather_service = s.id WHERE w.id = $1", [req.params.id_widget], (err, result) => {
+    pool.getPool().query("SELECT w.city, w.celsius, s.api_key FROM city_meteo_weather w INNER JOIN weather_service s ON w.id_weather_service = s.id WHERE w.id = $1 AND s.id_user = $2", [req.params.id_widget, req.user.user_id], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"})
+                return;
+            }
             widgetInfos = result.rows[0];
             const infos = {city: widgetInfos.city, celsius: widgetInfos.celsius}
             weather.setLang('en');
@@ -60,16 +75,43 @@ router.get('/weather/city-meteo/:id_widget', JWTService.authenticateToken, funct
 })
 
 router.post('/weather/city-meteo/', JWTService.authenticateToken, function (req, res) {
-    console.log(req.body);
     pool.getPool().query("INSERT INTO city_meteo_weather (id_weather_service, activate, city, celsius) VALUES ((SELECT id FROM weather_service WHERE id_user = $1), $2, $3, $4) RETURNING id", [req.user.user_id, req.body.activated, req.body.city, req.body.celsius], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"});
         } else {
+            if (!result.rows.length) {
+                res.status(401);
+                res.json({message: "Unauthorized"});
+                return;
+            }
             res.status(200);
             res.json({id: result.rows[0].id});
         }
     })
+})
+
+router.get('/weather/city-meteo/:id_widget/params', JWTService.authenticateToken, function (req, res) {
+
+    let widgetInfos;
+
+    pool.getPool().query("SELECT w.city, w.celsius, w.activate FROM city_meteo_weather w INNER JOIN weather_service s ON w.id_weather_service = s.id WHERE w.id = $1 AND s.id_user = $2", [req.params.id_widget, req.user.user_id], (err, result) => {
+        if (err) {
+            res.status(503);
+            res.json({message: "Service Unavailable"})
+        } else {
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                widgetInfos = result.rows[0];
+                const infos = {activate: widgetInfos.activate, city: widgetInfos.city, celsius: widgetInfos.celsius}
+                res.status(200)
+                res.json(infos)
+            }
+        }
+    })
+
 })
 
 module.exports = router;
