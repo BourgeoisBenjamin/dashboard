d1d966b2fc16ab9165d393789767e2f87ed6b567
Benjamin Bourgeois <benjishift@gmail.com>
2020-11-23 11:21:46 +0100

    :rocket: deploy route in order to get actual params of twitter widgets and add security level

--- server/routes/service/twitter-widget-routes.js
+++ server/routes/service/twitter-widget-routes.js
@@ -4,12 +4,17 @@ const oauth = require('../../services/oauth');
 const router = require('express').Router();
 
 router.delete('/twitter/last-tweets/:id_widget', JWTService.authenticateToken, function (req, res) {
-    pool.getPool().query("DELETE FROM last_tweets_twitter WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2)", [req.params.id_widget, req.user.user_id], (err, result) => {
+    pool.getPool().query("DELETE FROM last_tweets_twitter WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2) RETURNING *", [req.params.id_widget, req.user.user_id], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            res.status(200);
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+            }
         }
     })
 })
@@ -22,12 +27,17 @@ router.put('/twitter/last-tweets/:id_widget', JWTService.authenticateToken, func
         return;
     }
 
-    pool.getPool().query("UPDATE last_tweets_twitter SET activate = $3, number_tweets = $4 WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2)", [req.params.id_widget, req.user.user_id, req.query.activated, req.query.number_tweets], (err, result) => {
+    pool.getPool().query("UPDATE last_tweets_twitter SET activate = $3, number_tweets = $4 WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2) RETURNING id", [req.params.id_widget, req.user.user_id, req.query.activated, req.query.number_tweets], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            res.status(200);
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+            }
         }
     })
 })
@@ -36,23 +46,27 @@ router.get('/twitter/last-tweets/:id_widget', JWTService.authenticateToken, func
 
     let widgetInfos;
 
-    pool.getPool().query("SELECT l.number_tweets, s.token, s.tokensecret, s.twitter_id FROM last_tweets_twitter l INNER JOIN twitter_service s ON l.id_twitter_service = s.id WHERE l.id = $1", [req.params.id_widget], (err, result) => {
+    pool.getPool().query("SELECT l.number_tweets, s.token, s.tokensecret, s.twitter_id FROM last_tweets_twitter l INNER JOIN twitter_service s ON l.id_twitter_service = s.id WHERE l.id = $1  AND s.user_id = $2", [req.params.id_widget], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            widgetInfos = result.rows[0];
-            oauth.getOAuth().get(
-                'https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=' + widgetInfos.twitter_id + '&count=' + widgetInfos.number_tweets,
-                widgetInfos.token,
-                widgetInfos.tokensecret, (e, data) => {
-                    const tweets = JSON.parse(data);
-                    res.status(200)
-                    res.json(tweets)
-                })
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                widgetInfos = result.rows[0];
+                oauth.getOAuth().get(
+                    'https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=' + widgetInfos.twitter_id + '&count=' + widgetInfos.number_tweets,
+                    widgetInfos.token,
+                    widgetInfos.tokensecret, (e, data) => {
+                        const tweets = JSON.parse(data);
+                        res.status(200)
+                        res.json(tweets)
+                    })
+            }
         }
     })
-
 })
 
 router.post('/twitter/last-tweets/', JWTService.authenticateToken, function (req, res) {
@@ -68,19 +82,51 @@ router.post('/twitter/last-tweets/', JWTService.authenticateToken, function (req
             res.status(503);
             res.json({message: "Service Unavailable"});
         } else {
-            res.status(200);
-            res.json({id: result.rows[0].id});
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+                res.json({id: result.rows[0].id});
+            }
+        }
+    })
+})
+
+router.get('/twitter/last-tweets/:id_widget/params', JWTService.authenticateToken, function (req, res) {
+
+    let widgetInfos;
+
+    pool.getPool().query("SELECT l.number_tweets, l.activate FROM last_tweets_twitter l INNER JOIN twitter_service s ON l.id_twitter_service = s.id WHERE l.id = $1 AND s.user_id = $2", [req.params.id_widget, req.user.user_id], (err, result) => {
+        if (err) {
+            res.status(503);
+            res.json({message: "Service Unavailable"})
+        } else {
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                widgetInfos = result.rows[0];
+                const infos = {activate: widgetInfos.activate, number_tweets: widgetInfos.number_tweets}
+                res.status(200)
+                res.json(infos)
+            }
         }
     })
 })
 
 router.delete('/twitter/search-tweets/:id_widget', JWTService.authenticateToken, function (req, res) {
-    pool.getPool().query("DELETE FROM search_tweets_twitter WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2)", [req.params.id_widget, req.user.user_id], (err, result) => {
+    pool.getPool().query("DELETE FROM search_tweets_twitter WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2) RETURNING *", [req.params.id_widget, req.user.user_id], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            res.status(200);
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+            }
         }
     })
 })
@@ -93,12 +139,17 @@ router.put('/twitter/search-tweets/:id_widget', JWTService.authenticateToken, fu
         return;
     }
 
-    pool.getPool().query("UPDATE search_tweets_twitter SET activate = $3, search = $4, number_tweets = $5 WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2)", [req.params.id_widget, req.user.user_id, req.query.activated, req.query.number_tweets, req.query.search], (err, result) => {
+    pool.getPool().query("UPDATE search_tweets_twitter SET activate = $3, search = $4, number_tweets = $5 WHERE id = $1 AND id_twitter_service IN (SELECT id FROM twitter_service WHERE id_user = $2) RETURNING id", [req.params.id_widget, req.user.user_id, req.query.activated, req.query.number_tweets, req.query.search], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            res.status(200);
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+            }
         }
     })
 })
@@ -107,23 +158,27 @@ router.get('/twitter/search-tweets/:id_widget', JWTService.authenticateToken, fu
 
     let widgetInfos;
 
-    pool.getPool().query("SELECT l.number_tweets, l.search, s.token, s.tokensecret, s.twitter_id FROM search_tweets_twitter l INNER JOIN twitter_service s ON l.id_twitter_service = s.id WHERE l.id = $1", [req.params.id_widget], (err, result) => {
+    pool.getPool().query("SELECT l.number_tweets, l.search, s.token, s.tokensecret, s.twitter_id FROM search_tweets_twitter l INNER JOIN twitter_service s ON l.id_twitter_service = s.id WHERE l.id = $1 AND s.user_id = $2", [req.params.id_widget, req.user.user_id], (err, result) => {
         if (err) {
             res.status(503);
             res.json({message: "Service Unavailable"})
         } else {
-            widgetInfos = result.rows[0];
-            oauth.getOAuth().get(
-                'https://api.twitter.com/1.1/search/tweets.json?q=' + widgetInfos.search + '&count=' + widgetInfos.number_tweets,
-                widgetInfos.token,
-                widgetInfos.tokensecret, (e, data) => {
-                    const tweets = JSON.parse(data);
-                    res.status(200)
-                    res.json(tweets.statuses)
-                })
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                widgetInfos = result.rows[0];
+                oauth.getOAuth().get(
+                    'https://api.twitter.com/1.1/search/tweets.json?q=' + widgetInfos.search + '&count=' + widgetInfos.number_tweets,
+                    widgetInfos.token,
+                    widgetInfos.tokensecret, (e, data) => {
+                        const tweets = JSON.parse(data);
+                        res.status(200)
+                        res.json(tweets.statuses)
+                    })
+            }
         }
     })
-
 })
 
 router.post('/twitter/search-tweets/', JWTService.authenticateToken, function (req, res) {
@@ -139,8 +194,35 @@ router.post('/twitter/search-tweets/', JWTService.authenticateToken, function (r
             res.status(503);
             res.json({message: "Service Unavailable"});
         } else {
-            res.status(200);
-            res.json({id: result.rows[0].id});
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                res.status(200);
+                res.json({id: result.rows[0].id});
+            }
+        }
+    })
+})
+
+router.get('/twitter/search-tweets/:id_widget/params', JWTService.authenticateToken, function (req, res) {
+
+    let widgetInfos;
+
+    pool.getPool().query("SELECT l.number_tweets, l.search FROM search_tweets_twitter l INNER JOIN twitter_service s ON l.id_twitter_service = s.id WHERE l.id = $1 AND s.user_id = $2", [req.params.id_widget, req.user.user_id], (err, result) => {
+        if (err) {
+            res.status(503);
+            res.json({message: "Service Unavailable"})
+        } else {
+            if (!result.rows.length) {
+                res.status(401)
+                res.json({message: "Unauthorized"});
+            } else {
+                widgetInfos = result.rows[0];
+                const infos = {activate: widgetInfos.activate, search: widgetInfos.search, number_tweets: widgetInfos.number_tweets}
+                res.status(200)
+                res.json(infos)
+            }
         }
     })
 })
