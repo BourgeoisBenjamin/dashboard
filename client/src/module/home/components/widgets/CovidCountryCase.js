11dde7067b353889a3653ab18697a13ffac77480
Amaury <amaury.lecomte@epitech.eu>
2020-11-24 09:22:40 +0100

    :lipstick: Improved country case covid widget ui

--- client/src/module/home/components/widgets/CovidCountryCase.js
+++ client/src/module/home/components/widgets/CovidCountryCase.js
@@ -3,8 +3,12 @@ import {FiSettings} from "react-icons/fi";
 import CountryCaseModel from "../../../../core/models/services/covid/response/CountryCaseModel";
 import CovidService from "../../../../core/services/services/CovidService";
 import './CovidCountryCase.css'
-import CovidImage from '../../../../assets/images/covid.png'
+import LocationImage from '../../../../assets/images/placeholder.png'
+import DeathImage from '../../../../assets/images/death.png'
+import RecoveryImage from '../../../../assets/images/recovery.png'
+import ConfirmedImage from '../../../../assets/images/confirmed.png'
 import history from "../../../../history";
+import HumidityImage from "../../../../assets/images/humidity.png";
 
 class CovidCountryCase extends Component
 {
@@ -42,7 +46,6 @@ class CovidCountryCase extends Component
 
     onClickParameters()
     {
-        // history.push('/home/widget/covid/country-case/' + this.props.id);
         history.push({
             pathname: '/home/widget/covid/country-case/',
             search: '?id=' + this.props.id
@@ -54,25 +57,63 @@ class CovidCountryCase extends Component
             <div id="covid-country-case">
                 <div className="content">
                     <div className="header">
-                        <div className="covid-logo">
-                            <img src={CovidImage} />
-                        </div>
                         <div className="title">
-                            <p>Case per country</p>
+                            <div className="main-title">
+                                <p>COVID 19</p>
+                            </div>
+                            <div className="second-title">
+                                <p>Summary per country</p>
+                            </div>
                         </div>
                         <div className="logo-parameters" onClick={this.onClickParameters}>
                             <FiSettings color="white" size={30}/>
                         </div>
                     </div>
                     <div className="content">
-                        <div className="description">
-                            <p>In {this.state.model.Country}, {new Date(this.state.model.Date).toLocaleString()}</p>
+                        <div className="header-content">
+                            <div className="description">
+                                <div className="logo">
+                                    <img src={LocationImage}/>
+                                </div>
+                                <div className="text">
+                                    <p>In {this.state.model.Country}, {new Date(this.state.model.Date).toLocaleString()}</p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="total-deaths">
-                            <p>Total deaths : {this.state.model.Deaths}</p>
-                        </div>
-                        <div className="total-recovered">
-                            <p>Total recovered : {this.state.model.Recovered}</p>
+                        <div className="wrap-elements">
+                            <div className="element">
+                                <div className="icon">
+                                    <img alt="" src={ConfirmedImage}/>
+                                </div>
+                                <div className="description">
+                                    <p>Total confirmed</p>
+                                </div>
+                                <div className="value">
+                                    <p>{this.state.model.Confirmed}</p>
+                                </div>
+                            </div>
+                            <div className="element">
+                                <div className="icon">
+                                    <img alt="" src={RecoveryImage}/>
+                                </div>
+                                <div className="description">
+                                    <p>Total recovered</p>
+                                </div>
+                                <div className="value">
+                                    <p>{this.state.model.Recovered}</p>
+                                </div>
+                            </div>
+                            <div className="element">
+                                <div className="icon">
+                                    <img alt="" src={DeathImage}/>
+                                </div>
+                                <div className="description">
+                                    <p>Total deaths</p>
+                                </div>
+                                <div className="value">
+                                    <p>{this.state.model.Deaths}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
