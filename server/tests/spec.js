a29691b882a09904664fd0a8a7310f791254d778
Benjamin Bourgeois <benjishift@gmail.com>
2020-11-28 16:27:50 +0100

    :white_check_mark: add 5 tests

--- server/tests/spec.js
+++ server/tests/spec.js
@@ -1,14 +1,17 @@
 let chai = require('chai')
 let chaiHttp = require('chai-http');
+const pool = require('../services/postgresql');
+
 
 chai.use(chaiHttp);
 
 describe('loading express server', function () {
     var server;
-    beforeEach(function () {
+    before(async function () {
         server = require('./../server.test');
+        await pool.getPool().query('TRUNCATE users CASCADE')
     });
-    afterEach(function () {
+    after(function () {
         server.close();
     });
     it('test server', (done) => {
@@ -19,5 +22,91 @@ describe('loading express server', function () {
                 chai.expect(res.body.message).to.equal('Hello world !!')
                 done();
             });
+    });
+});
+
+describe('register a user', function () {
+    var server;
+    before(async function () {
+        server = require('./../server.test');
+        await pool.getPool().query('TRUNCATE users CASCADE')
+    });
+    after(function () {
+        server.close();
+    });
+    it('register user', (done) => {
+
+        chai.request(server)
+            .post('/account/register')
+            .send({username: 'benjaminbourgeois', password: 'test', email: 'benjishift@gmail.com'})
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200)
+                done();
+            });
+    });
+});
+
+describe('register existing user', function () {
+
+    var server;
+
+    before(async function () {
+        server = require('./../server.test');
+        await pool.getPool().query('TRUNCATE users CASCADE')
+    });
+    after(function () {
+        server.close();
+    });
+    it('register existing user', (done) => {
+
+        chai.request(server)
+            .post('/account/register')
+            .send({username: 'benjaminbourgeois', password: 'test', email: 'benjishift@gmail.com'})
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200)
+            });
+        chai.request(server)
+            .post('/account/register')
+            .send({username: 'benjaminbourgeois', password: 'test', email: 'benjishift@gmail.com'})
+            .end((err, res) => {
+                chai.expect(res).to.have.status(409)
+                done();
+            });
+
+    });
+});
+
+describe('login', function () {
+
+    var server;
+
+    before(async function () {
+        server = require('./../server.test');
+        await pool.getPool().query('TRUNCATE users CASCADE')
+    });
+    after(function () {
+        server.close();
+    });
+    it('register a user', (done) => {
+
+        chai.request(server)
+            .post('/account/register')
+            .send({username: 'benjaminbourgeois', password: 'test', email: 'benjishift@gmail.com'})
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200)
+                done()
+            });
+
+    });
+    it('login a user', (done) => {
+
+        chai.request(server)
+            .post('/account/login')
+            .send({identifier: 'benjishift@gmail.com', password: 'test'})
+            .end((err, res) => {
+                chai.expect(res).to.have.status(200)
+                done();
+            });
+
     });
 });
